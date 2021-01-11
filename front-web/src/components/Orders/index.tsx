4dc118312b6a326339516cffe88ff4ad7bac2d29
Marcelo Santos <rocha.mer21@gmail.com>
2021-01-11 13:37:04 -0300

    Add card and api via axios

--- front-web/src/components/Orders/index.tsx
+++ front-web/src/components/Orders/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react'
+
 import './styles.css'
 
 import StepsHeader from './Steps-Header'
+import ProductsList from './ProductsList'
+import { Product } from './types'
+import { fetchProducts } from '../../api'
 
 export default function Orders() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className='orders-container'>
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   )
 }
