946747f464aacc0f5f0655b4606c5d57187bb367
Marcelo Santos <rocha.mer21@gmail.com>
2021-01-13 14:26:30 -0300

    Add orders list

--- mobile/src/components/Orders/index.tsx
+++ mobile/src/components/Orders/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import Header from '../Header'
+import OrderCard from '../OrderCard'
 
 export default function Orders() {
   const handleOnPress = () => {}
@@ -8,11 +9,23 @@ export default function Orders() {
   return (
     <>
       <Header />
-      <View>
-        <Text>Orders</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+      </ScrollView>
     </>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%',
+  },
+})
