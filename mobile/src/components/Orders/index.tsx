d3e6fefeccb1124e4d5db0caadfeacb92b617a3b
Marcelo Santos <rocha.mer21@gmail.com>
2021-01-13 07:26:15 -0300

    Add Routes

---
+++ mobile/src/components/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Header from '../Header'
+
+export default function Orders() {
+  const handleOnPress = () => {}
+
+  return (
+    <>
+      <Header />
+      <View>
+        <Text>Orders</Text>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({})
